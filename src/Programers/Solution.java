b0ae85bea4dbb10738e13c5608081517fbc3528e
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-12-08 19:05:59 +0900

    가운데 글자 가져오기 3진법 뒤집기

--- src/Programers/Solution.java
+++ src/Programers/Solution.java
@@ -2,36 +2,23 @@ package Programers;
 
 class Solution {
 
-    public int solution(int n, int[] lost, int[] reserve) {
-        int[] student = new int[n + 2];
-
-        for (int i = 0; i < student.length; i++) {
-            student[i] = 1;
+    public int solution(int n) {
+        String ternaryNotation = "";
+        int answer = 0;
+        while (n > 0) {
+            ternaryNotation += n % 3;
+            n /= 3;
         }
-        for (int lostStudent : lost) {
-            student[lostStudent] -= 1;
+        ternaryNotation = Long.toString(Long.parseLong(ternaryNotation));
+        int powValue = ternaryNotation.length()-1;
+        for(int i=0;i<ternaryNotation.length();i++){
+            answer += Integer.parseInt(ternaryNotation.substring(i,i+1)) * Math.pow(3,powValue--);
         }
-        for (int reserveEnableStudent : reserve) {
-            student[reserveEnableStudent] += 1;
-        }
-        for (int i = 1; i < student.length - 1; i++) {
-            if (student[i] == 0) {
-                if (student[i - 1] == 2) {  // 앞에서 빌리는 경우
-                    student[i - 1] = 1;
-                    continue;
-                }
-                if (student[i + 1] == 2) {  // 뒤에서 빌리는 경우
-                    student[i + 1] = 1;
-                    continue;
-                }
-                n -= 1;                     // 빌리지 못하는 경우
-            }
-        }
-        return n;
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution tmp = new Solution();
-        System.out.println(tmp.solution(5, new int[]{1, 2, 3, 4, 5}, new int[]{1}));
+        System.out.println(tmp.solution(100000000));
     }
 }

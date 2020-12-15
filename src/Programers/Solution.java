f2fad9d07e3ba08f157faeb664db90d8635e5405
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-12-15 23:12:58 +0900

    두 개 뽑아서 더하기

--- src/Programers/Solution.java
+++ src/Programers/Solution.java
@@ -1,16 +1,6 @@
 package Programers;
 
-import java.util.regex.Pattern;
 
 class Solution {
-    public boolean solution(String s) {
-        String solutionPattern = "[0-9]{4}|[0-9]{6}";
-        return Pattern.matches(solutionPattern,s);
-    }
 
-    public static void main(String[] args) {
-        Solution ans = new Solution();
-        String parameter = "1234";
-        System.out.println(ans.solution("123456"));
-    }
 }

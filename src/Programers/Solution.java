e0c390cdd1edb6ee9d386f906822113025bdd0d8
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-12-17 22:59:58 +0900

    정수 제곱근 판별

--- src/Programers/Solution.java
+++ src/Programers/Solution.java
@@ -2,7 +2,16 @@ package Programers;
 
 
 class Solution {
-    public int solution(String s) {
-        return Integer.parseInt(s);
+
+    public long solution(long n) {
+        if (Math.pow((int) Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution tmp = new Solution();
+        System.out.println(tmp.solution(121));
     }
 }

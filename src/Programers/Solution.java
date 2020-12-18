cc57afef2fb54c773719e51aee4f5cc47fe86c65
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-12-18 12:23:11 +0900

    시저 암호

--- src/Programers/Solution.java
+++ src/Programers/Solution.java
@@ -3,15 +3,25 @@ package Programers;
 
 class Solution {
 
-    public long solution(long n) {
-        if (Math.pow((int) Math.sqrt(n), 2) == n) {
-            return (long) Math.pow(Math.sqrt(n) + 1, 2);
+    class Caesar {
+
+        public String caesar(String s, int _n) {
+            return s.chars().map(c -> {
+                int n = _n % 26;
+                if (c >= 'a' && c <= 'z') {
+                    return 'a' + (c - 'a' + n) % 26;
+                } else if (c >= 'A' && c <= 'Z') {
+                    return 'A' + (c - 'A' + n) % 26;
+                } else {
+                    return c;
+                }
+            }).mapToObj(c -> String.valueOf((char) c))
+                .reduce((a, b) -> a + b).orElse("");
         }
-        return -1;
     }
 
     public static void main(String[] args) {
         Solution tmp = new Solution();
-        System.out.println(tmp.solution(121));
+
     }
 }
